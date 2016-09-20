1398968bfe2b600c1f2eb9f0cbc4212485482e8d
Jayce Dowell <jayce.dowell@gmail.com>
2016-09-20 19:33:44 +0000

    Added in the SmartCopy server software.

---
+++ smartCommon.py
@@ -0,0 +1,325 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+import sys
+import time
+import uuid
+import select
+import threading
+import traceback
+import subprocess
+import logging
+try:
+	import cStringIO as StringIO
+except ImportError:
+	import StringIO
+
+from collections import OrderedDict
+
+__version__ = '0.1'
+__revision__ = '$Rev$'
+__all__ = ['SerialNumber', 'LimitedSizeDict', 'InterruptibleCopy', '__version__', '__revision__', '__all__']
+
+
+smartCommonLogger = logging.getLogger('__main__')
+
+
+class SerialNumber(object):
+	"""
+	Simple class for a serial number generator.
+	"""
+	
+	def __init__(self):
+		self.sn = 1
+		self.lock = threading.Semaphore()
+		
+	def get(self):
+		self.lock.acquire()
+		out = self.sn*1
+		self.sn += 1
+		self.lock.release()
+		
+		return out
+		
+	def reset(self):
+		self.lock.acquire()
+		self.sn = 1
+		self.lock.release()
+		
+		return True
+
+
+class LimitedSizeDict(OrderedDict):
+	"""
+	From:
+		https://stackoverflow.com/questions/2437617/limiting-the-size-of-a-python-dictionary
+	"""
+	
+	def __init__(self, *args, **kwds):
+		self.size_limit = kwds.pop("size_limit", None)
+		OrderedDict.__init__(self, *args, **kwds)
+		self._check_size_limit()
+		
+	def __setitem__(self, key, value):
+		OrderedDict.__setitem__(self, key, value)
+		self._check_size_limit()
+		
+	def _check_size_limit(self):
+		if self.size_limit is not None:
+			while len(self) > self.size_limit:
+				self.popitem(last=False)
+
+
+class InterruptibleCopy(object):
+	_rsyncRE = re.compile('.*?(?P<transferred>\d) +(?P<progress>\d{1,3}%) +(?P<speed>\d+\.\d+[ kMG]B/s) +(?P<remaining>.*)')
+	
+	def __init__(self, host, hostpath, dest, destpath, id=None):
+		# Copy setup
+		self.host = host
+		self.hostpath = hostpath
+		self.dest = dest
+		self.destpath = destpath
+		
+		# Identifier
+		if id is None:
+			id = str( uuid.uuid4() )
+		self.id = id
+		
+		# Thread setup
+		self.thread = None
+		self.process= None
+		self.stdout, self.stderr = '', ''
+		self.status = ''
+		
+		# Start the copy running
+		self.resume()
+		
+	def __str__(self):
+		return "%s = %s:%s -> %s:%s" % (self.id, self.host, self.hostpath, self.dest, self.destpath)
+		
+	def getStatus(self):
+		"""
+		Return the status of the copy.
+		"""
+		
+		return self.status
+		
+	def getBytesTransferred(self):
+		"""
+		Return the number of bytes transferred.
+		"""
+		
+		mtch = self._rsyncRE.search(self.stdout)
+		if mtch is not None:
+			trans = mtch.gr('transferred')
+		else:
+			trans = "0"
+			
+		return trans
+		
+	def getProgress(self):
+		"""
+		Return the percentage progress of the copy.
+		"""
+		
+		mtch = self._rsyncRE.search(self.stdout)
+		if mtch is not None:
+			prog = mtch.group('progress')
+		else:
+			prog = '0%'
+			
+		return prog
+		
+	def getSpeed(self):
+		"""
+		Return the current copy speed or 'paused' if the copy is paused.
+		"""
+		
+		if self.isRunning:
+			mtch = self._rsyncRE.search(self.stdout)
+			if mtch is not None:
+				speed = mtch.group('speed')
+			else:
+				speed = '0.00kB/s'
+		else:
+			speed = 'paused'
+			
+		return speed
+		
+	def getTimeRemaining(self):
+		"""
+		Return the estimated time remaining or 'unknown' if the copy is paused.
+		"""
+		
+		if self.isRunning:
+			mtch = self._rsyncRE.search(self.stdout)
+			if mtch is not None:
+				rema = mtch.group('remaining')
+			else:
+				rema = '99:59:59'
+		else:
+			rema = 'unknown'
+			
+		return rema
+		
+	def isRemote(self):
+		"""
+		Return a Boolean of whether or not the copy is to a remote host.
+		"""
+		
+		if self.host == self.dest:
+			return False
+		else:
+			return True
+			
+	def isRunning(self):
+		"""
+		Return a Boolean of whether or not the copy is currently running.
+		"""
+		
+		if self.thread is None:
+			return False
+		else:
+			if self.thread.isAlive():
+				return True
+			else:
+				return False
+				
+	def isComplete(self):
+		"""
+		Return a Boolean of whether or not the copy has finished.
+		"""
+		
+		if self.isRunning():
+			return False
+		elif self.status[:6] != 'paused':
+			return True
+		else:
+			return False
+			
+	def pause(self):
+		"""
+		Pause the copy.
+		"""
+		
+		if self.thread is not None:
+			self.process.kill()
+			self.thread.join()
+			
+			self.thread = None
+			self.process = None
+			self.status = 'paused'
+			
+			return True
+		else:
+			return False
+			
+	def resume(self):
+		"""
+		Resume the copy.
+		"""
+		
+		if self.thread is None:
+			self.thread = threading.Thread(target=self._run)
+			self.thread.setDaemon(1)
+			self.thread.start()
+			self.status = 'active'
+			
+			time.sleep(1)
+			
+			return True
+		else:
+			return False
+			
+	def cancel(self):
+		"""
+		Cancel the copy.
+		"""
+		
+		if self.isRunning:
+			self.pause()
+		self.status = 'canceled'
+		
+		return True
+		
+	def _getCommand(self):
+		"""
+		Build up a subprocess-compatible command needed to copy the data.
+		"""
+		
+		if self.host == '':
+			# Locally originating copy
+			cmd = ["rsync",  "-avH",  "--append", "--partial", "--progress", self.hostpath]
+			
+			if self.dest == '':
+				# Local destination
+				cmd.append( "%s" % self.destpath )
+			else:
+				# Remote destination
+				cmd.append( "%s:%s" % (self.dest, self.destpath) )
+				
+		else:
+			# Remotely originating copy
+			cmd = ["ssh", "-t", "-t", "mcsdr@%s" % self.host.lower()]
+			
+			if self.dest == self.host:
+				# Source and destination are on the same machine
+				cmd.append( 'shopt -s huponexit && rsync -avH --append --partial --progress %s %s' % (self.hostpath, self.destpath) )
+			else:
+				# Source and destination are on different machines
+				cmd.append( 'shopt -s huponexit && rsync -avH --append --partial --progress %s %s:%s' % (self.hostpath, self.dest, self.destpath) )
+				
+		return cmd
+		
+	def _run(self):
+		"""
+		Start the copy.
+		"""
+		
+		# Get the command to use
+		cmd = self._getCommand()
+		
+		# Start up the process and start looking at the stdout
+		self.process = subprocess.Popen(cmd, bufsize=1, stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
+		watchOut = select.poll()
+		watchOut.register(self.process.stdout)
+		smartCommonLogger.debug('Launched \'%s\' with PID %i', ' '.join(cmd), self.process.pid)
+		
+		# Read from stdout while the copy is running so we can get 
+		# an idea of what is going on with the progress
+		while True:
+			## Is there something to read?
+			stdout = ''
+			while watchOut.poll(1):
+				try:
+					stdout += self.process.stdout.read(1)
+				except ValueError:
+					break
+					
+			## Did we read something?  If not, sleep
+			if stdout == '':
+				time.sleep(1)
+			else:
+				self.stdout = stdout.rstrip()
+				
+			## Are we done?
+			self.process.poll()
+			if self.process.returncode is not None:
+				### Yes, break out of this endless loop
+				break
+				
+		# Pull out anything that might be stuck in the buffers
+		self.stdout, self.stderr = self.process.communicate()
+		
+		smartCommonLogger.debug('PID %i exited with code %i', self.process.pid, self.process.returncode)
+		
+		if self.process.returncode == 0:
+			self.status = 'complete'
+		elif self.process.returncode < 0:
+			self.status = 'paused'
+		else:
+			smartCommonLogger.debug('failed -> %s', self.stderr.rstrip())
+			self.status = 'error: %s' % self.stderr
+			
+		return self.process.returncode
---
+++ smartThreads.py
@@ -0,0 +1,491 @@
+# -*- coding: utf-8 -*-
+import os
+import re
+import sys
+import copy
+import time
+import uuid
+import Queue
+import select
+import socket
+import threading
+import traceback
+import subprocess
+import logging
+try:
+	import cStringIO as StringIO
+except ImportError:
+	import StringIO
+
+from smartCommon import *
+
+__version__ = "0.1"
+__revision__ = "$Rev$"
+__all__ = ['MonitorStation', 'ManageDR', '__version__', '__revision__', '__all__']
+
+
+smartThreadsLogger = logging.getLogger('__main__')
+
+
+# Serial number generator for the queue entries
+sng = SerialNumber()
+
+# Remote copy lock to help ensure that there is only one remove transfer at at time
+rcl = threading.Semaphore()
+
+
+class MonitorStation(object):
+	def __init__(self, mselog='/home/op1/MCS/sch/mselog.txt', SCCallbackInstance=None):
+		self.mselog = mselog
+		self.SCCallbackInstance = SCCallbackInstance
+		
+		# Setup threading
+		self.thread = None
+		self.alive = threading.Event()
+		self.lastError = None
+		
+		# Setup the data recorder busy list
+		self.busy = {}
+		for i in xrange(1, 5+1):
+			self.busy['DR%i' % i] = True
+			
+		# Setup the MCS command queue state
+		self.cmdQueue = LimitedSizeDict(size_limit=64)
+			
+	def start(self):
+		"""
+		Start the station monitoring thread.
+		"""
+		
+		if self.thread is not None:
+			self.stop()
+			
+		for dr in self.busy:
+			self.busy[dr] = True
+			if self.SCCallbackInstance is not None:
+				self.SCCallbackInstance.processDRStateChange(dr, self.busy[dr])
+				
+		self.thread = threading.Thread(target=self.pollStation, name='pollStation')
+		self.thread.setDaemon(1)
+		self.alive.set()
+		self.thread.start()
+		time.sleep(1)
+		
+		smartThreadsLogger.info('Started station monitoring background thread')
+		
+	def stop(self):
+		"""
+		Stop the station monitoring thread.
+		"""
+		
+		if self.thread is not None:
+			self.alive.clear()          #clear alive event for thread
+			
+			self.thread.join()          #don't wait too long on the thread to finish
+			self.thread = None
+			self.lastError = None
+			
+			for dr in self.busy:
+				self.busy[dr] = True
+				if self.SCCallbackInstance is not None:
+					self.SCCallbackInstance.processDRStateChange(dr, self.busy[dr])
+					
+			smartThreadsLogger.info('Stopped station monitoring background thread')
+			
+	def _parseLogData(self, lines):
+		# Make a copy of the current DR states
+		newBusyState = copy.deepcopy(self.busy)
+			
+		# Walk through the lines
+		for line in lines:
+			## Parse the line
+			fields = line.split(None, 9)
+			try:
+				rid = int(fields[5], 10)			# MCS reference ID
+				status = int(fields[6], 10)		# MCS command send status
+				subsys = fields[7]				# Target subsystem
+				cmd = fields[8]				# Command
+				data = fields[9].rsplit('|', 1)[0]	# Command arguments/response
+			except Exception as e:
+				smartThreadsLogger.debug("MonitorStation: error parsing log line '%s': %s", line.rstrip(), str(e))
+				continue
+				
+			## Ignore non-DR subsystems
+			if subsys[:2] != 'DR':
+				continue
+				
+			## Figure out what to do basedon the command send status
+			if status == 2:
+				### Save for later
+				self.cmdQueue[rid] = (rid, subsys, cmd, data)
+				
+			elif status == 3:
+				### The command as been responded to
+				if cmd in ('SHT', 'REC', 'SPC'):
+					### A valid shutdown, record, or spectrometer command
+					newBusyState[subsys] = True
+					
+				elif cmd in ('INI', 'STP',):
+					### A valid INI or stop command
+					newBusyState[subsys] = False
+					
+				elif cmd == 'RPT':
+					### A valid report command has been responded to
+					try:
+						rptType = self.cmdQueue[rid][3]
+						if rptType == 'OP-TYPE':
+							if data[:4] == 'Idle':
+								newBusyState[subsys] = False
+							else:
+								newBusyState[subsys] = True
+						elif rptType == 'SUMMARY':
+							if data[:6] != 'NORMAL':
+								newBusyState[subsys] = True
+					except KeyError:
+						pass
+						
+			elif status == 8:
+				## The subsystem is dead
+				newBusyState[subsys] = True
+				
+		return newBusyState
+		
+	def pollStation(self):
+		"""
+		Poll the mselog.txt file to look for changes in the data recorders states.
+		"""
+		
+		# Open the MCS logfile for reading and being watching for changes
+		tail = subprocess.Popen(['tail', '-F', self.mselog], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+		watch = select.poll()
+		watch.register(tail.stdout)
+		
+		# Go!
+		while self.alive.isSet():
+			try:
+				## Is there anything to read?
+				if watch.poll(1):
+					### Good, read in all that we can
+					lines = [tail.stdout.readline(),]
+					while watch.poll(1):
+						lines.append( tail.stdout.readline() )
+						
+					### Parse the lines to figure out the current state of the DRs
+					newState = self._parseLogData(lines)
+					
+					### Propogate any state changes we find
+					for dr in self.busy:
+						if newState[dr] != self.busy[dr]:
+							smartThreadsLogger.debug('State changed on %s to %s', dr, 'busy' if newState[dr] else 'idle')
+							self.busy[dr] = newState[dr]
+							if self.SCCallbackInstance is not None:
+								self.SCCallbackInstance.processDRStateChange(dr, self.busy[dr])
+								
+			except Exception as e:
+				exc_type, exc_value, exc_traceback = sys.exc_info()
+				smartThreadsLogger.error("MonitorStation: pollStation failed with: %s at line %i", str(e), traceback.tb_lineno(exc_traceback))
+				
+				## Grab the full traceback and save it to a string via StringIO
+				fileObject = StringIO.StringIO()
+				traceback.print_tb(exc_traceback, file=fileObject)
+				tbString = fileObject.getvalue()
+				fileObject.close()
+				## Print the traceback to the logger as a series of DEBUG messages
+				for line in tbString.split('\n'):
+					smartThreadsLogger.debug("%s", line)
+					
+			## Sleep for a bit to wait on new log entries
+			time.sleep(1)
+			
+		# Clean up and get ready to exit
+		watch.unregister(tail.stdout)
+		tail.kill()
+		
+		# Done
+		return True
+		
+	def getState(self, dr):
+		"""
+		Return the current busy state of the specified data recorder.  A 
+		two-element tuple of result and value are returned.  If the data 
+		recorder is out of range or some other error occurs, 
+		(False, errorString) is returned.  Otherwise (True, status) is 
+		returned.
+		"""
+		
+		try:
+			return True, 'busy' if self.busy[dr] else 'idle'
+		except KeyError:
+			return False, 'Unknown data recorder %s' % str(dr)
+
+
+class ManageDR(object):
+	def __init__(self, dr, SCCallbackInstance=None):
+		self.dr = dr
+		self.SCCallbackInstance = SCCallbackInstance
+		
+		self.queue = Queue.Queue()
+		
+		# Setup threading
+		self.thread = None
+		self.alive = threading.Event()
+		self.alive.clear()
+		self.lastError = None
+		
+		# Activity
+		self.inhibit = True
+		self.active = None
+		
+		# Results cache
+		self.results = LimitedSizeDict(size_limit=512)
+		
+	def start(self):
+		"""
+		Start the station monitoring thread.
+		"""
+		
+		if self.thread is not None:
+			self.stop()
+			
+		self.thread = threading.Thread(target=self.processQueue, name='processQueue%s' % self.dr)
+		self.thread.setDaemon(1)
+		self.alive.set()
+		self.thread.start()
+		time.sleep(1)
+		
+		smartThreadsLogger.info('Started smart copy processing queue for %s' % self.dr)
+		
+	def stop(self):
+		"""
+		Stop the station monitoring thread.
+		"""
+		
+		if self.thread is not None:
+			self.pause()                #stop the current copy
+			try:
+				rcl.release()
+			except threading.ThreadError:
+				pass
+				
+			self.alive.clear()          #clear alive event for thread
+			
+			self.thread.join()          #don't wait too long on the thread to finish
+			self.thread = None
+			self.lastError = None
+			
+			smartThreadsLogger.info('Stopped smart copy processing queue for %s' % self.dr)
+			
+	def pause(self):
+		"""
+		Pause the current copy and the queue processing.
+		"""
+		
+		try:
+			self.active.pause()
+			self.results[self.active.id] = 'paused for %s:%s -> %s:%s' % (self.active.host, self.active.hostpath, self.active.dest, self.active.destpath)
+			
+		except AttributeError:
+			pass
+			
+		self.inhibit = True
+		
+		return True, 0
+		
+	def resume(self):
+		"""
+		Resume the current copy and the queue processing.
+		"""
+		
+		try:
+			self.active.resume()
+			self.results[self.active.id] = 'active/resumed for %s:%s -> %s:%s' % (self.active.host, self.active.hostpath, self.active.dest, self.active.destpath)
+			
+		except AttributeError:
+			pass
+			
+		self.inhibit = False
+		
+		return True, 0
+		
+	def processQueue(self):
+		
+		while self.alive.isSet():
+			tStart = time.time()
+			
+			try:
+				# Determine if we are ready to proceed
+				readyToProcess = True
+				## Are we paused for some reason?
+				if self.inhibit:
+					readyToProcess &= False
+				## Is there already an item being process?
+				if self.active is not None:
+					## Has the tasked finished?
+					if not self.active.isComplete():
+						### Nope, no need to start a new one
+						readyToProcess &= False
+					else:
+						### Yes, save the output
+						self.results[self.active.id] = self.active.status
+						### Check to see if this was a remote copy.  If so, 
+						### we need to release the lock
+						if self.active.isRemote():
+							rcl.release()
+							
+				if readyToProcess:
+					# It looks like we can try to start another item in the queu running
+					try:
+						## Pull out the next task
+						task = self.queue.get(False, 5)
+						if task is not None:
+							### Make sure the task hasn't been canceled
+							if self.results[task[-1]] != 'canceled':
+								### If the copy appears to be remote, make sure that we can get 
+								### the network lock.  If we can't, add the task back to the end
+								### of the queue and skip to the next iteration
+								if task[0] != task[2]:
+									if not rcl.acquire(False):
+										self.queue.put(task)
+										time.sleep(5)
+										continue
+										
+								### If we've made it this far we have a copy that is ready to go.  
+								### Start it up.
+								self.active = InterruptibleCopy(*task)
+								self.results[self.active.id] = 'active/started for %s:%s -> %s:%s' % (task[0], task[1], task[2], task[3])
+								
+					except Queue.Empty:
+						self.active = None
+						
+			except Exception as e:
+				exc_type, exc_value, exc_traceback = sys.exc_info()
+				smartThreadsLogger.error("ManageDR: processQueue failed with: %s at line %i", str(e), traceback.tb_lineno(exc_traceback))
+				
+				## Grab the full traceback and save it to a string via StringIO
+				fileObject = StringIO.StringIO()
+				traceback.print_tb(exc_traceback, file=fileObject)
+				tbString = fileObject.getvalue()
+				fileObject.close()
+				## Print the traceback to the logger as a series of DEBUG messages
+				for line in tbString.split('\n'):
+					smartThreadsLogger.debug("%s", line)
+					
+			time.sleep(5)
+			
+	def addCopyCommand(self, host, hostpath, dest, destpath):
+		"""
+		Add a copy command to the queue and return the ID.
+		"""
+		
+		id = str( sng.get() )
+		
+		try:
+			self.queue.put( (host, hostpath, dest, destpath, id) )
+			self.results[id] = 'queued for %s:%s -> %s:%s' % (host, hostpath, dest, destpath)
+			return True,  id
+		except Exception as e:
+			return False, str(e)
+			
+	def cancelCopyCommand(self, id):
+		"""
+		Cancel a queued copy command.
+		"""
+		
+		try:
+			if self.active is not None:
+				if self.active.id == id:
+					self.active.cancel()
+			self.results[id] = 'canceled'
+			return True, id
+		except KeyError:
+			return False, 'Unknown copy command ID'
+			
+	def getCopyCommand(self, id):
+		"""
+		Return the status of the copy command.
+		"""
+		
+		try:
+			status = self.results[id]
+			return True, status
+		except KeyError:
+			return False, 'Unknown copy command ID'
+			
+	def getQueueSize(self):
+		"""
+		Return the size of the copy queue.
+		"""
+		
+		return True, self.queue.qsize()
+		
+	def getQueueState(self):
+		"""
+		Return the state of the copy queue.
+		"""
+		
+		return True, 'active' if not self.inhibit else 'paused'
+		
+	def getActiveID(self):
+		"""
+		Return the active copy ID.
+		"""
+		
+		if self.active is not None:
+			return True, self.active.id
+		else:
+			return True, 'None'
+			
+	def getActiveStatus(self):
+		"""
+		Return the status of the active copy.
+		"""
+		
+		if self.active is not None:
+			try:
+				status = self.results[self.active.id]
+				return True, status
+			except KeyError:
+				return False, 'Cannot access active command ID'
+		else:
+			return True, 'None'
+			
+	def getActiveBytesTransferred(self):
+		"""
+		Return the number of bytes transferred by the active copy.
+		"""
+		
+		if self.active is not None:
+			return True, self.active.getBytesTransferred()
+		else:
+			return True, 'None'
+			
+	def getActiveProgress(self):
+		"""
+		Return the percentage progress for the active copy.
+		"""
+		
+		if self.active is not None:
+			return True, self.active.getProgress()
+		else:
+			return True, 'None'
+			
+	def getActiveSpeed(self):
+		"""
+		Return the speed of the active copy.
+		"""
+		
+		if self.active is not None:
+			return True, self.active.getSpeed()
+		else:
+			return True, 'None'
+			
+	def getActiveTimeRemaining(self):
+		"""
+		Return the last rsync output line.
+		"""
+		
+		if self.active is not None:
+			return True, self.active.getTimeRemaining()
+		else:
+			return True, 'None'
+			
