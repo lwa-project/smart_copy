631546b0f8fa3b074129827553381dc14e826e4d
jaycedowell <jdowell@unm.edu>
2024-03-04 16:04:52 -0700

    Send JSON data.

--- scripts/postStatus.py
+++ scripts/postStatus.py
@@ -2,9 +2,12 @@
 
 import os
 import sys
+import json
+import pytz
 import time
 import subprocess
 from socket import gethostname
+from datetime import datetime
 
 from lwa_auth import KEYS as LWA_AUTH_KEYS
 from lwa_auth.signed_requests import post as signed_post
@@ -12,6 +15,16 @@ from lwa_auth.signed_requests import post as signed_post
 URL = "https://lwalab.phys.unm.edu/OpScreen/update"
 SITE = gethostname().split('-', 1)[0]
 PATH = os.path.dirname(os.path.abspath(__file__))
+
+UTC = pytz.UTC
+
+def _serialize_datetime(value):
+    try:
+        if value.tzinfo is not None:
+            value = value.astimezone(UTC)
+        return value.isoformat() + 'Z'
+    except AttributeError:
+        return value
 
 summary = 'NORMAL'
 validDRs = (1, 2, 3, 4, 5)
@@ -46,9 +59,12 @@ for dr in validDRs:
         summary = 'ERROR'
         continue
         
-data = [time.time(), SITE, summary, total_count, active_count]
-data = [str(v) for v in data]
-data = ';;;'.join(data)
+data = [{'site': SITE,
+         'summary': summary, 
+         'total': total_count,
+         'active': activet_count,
+         'update': datetime.utcnow()},]
+data = json.dumps(data)
 f = signed_post(LWA_AUTH_KEYS.get(SITE+'-log', kind='private'), URL,
                 data={'site': 'elwa', 'subsystem': 'ASP', 'data': data})
 f.close()
